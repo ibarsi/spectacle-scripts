50ed113de58e295b7cb72bbc5966411cf68bee87
Ken Wheeler <thekenwheeler@gmail.com>
2017-11-09 12:12:07 -0500

    Updating spectacle-scripts

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Presentation from './presentation';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Presentation />, document.getElementById('root'));
 registerServiceWorker();
